d8463186dd960f385624fbc8fab82b79666dc872
Ben Lesh <blesh@netflix.com>
2015-05-01 10:57:00 -0700

    add example

---
+++ app/routes/index.js
@@ -0,0 +1,15 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model: function(){
+    return range(60).map(() => range(30).map(x => ({ x, y: Math.random() * 100 })));
+  }
+});
+
+function range(n) {
+  var result = [];
+  for(var i = 0; i < n; i++) {
+    result.push(i);
+  }
+  return result;
+}
